64bf00f421431c3aa1109e80cceab414927f27e3
Chris Oyler <christopher.oyler@gmail.com>
2015-05-20 14:11:55 -0700

    buttons pulling data from db

---
+++ scripts/clean-db.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [ -z "$1" ] ; then
+  echo "Enter a database name"
+  exit 1
+fi
+
+mongoimport --jsonArray --drop --db $1 --collection users --file ../db/users.json
+# mongoimport --jsonArray --drop --db laundry-dev --collection users --file ../db/users.json
+mongoimport --jsonArray --drop --db $1 --collection machines --file ../db/machines.json
+# mongoimport --jsonArray --drop --db laundry-dev --collection machines --file ../db/machines.json
